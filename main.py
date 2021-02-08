d153309a70fdd2388b9d20e7cafa4d3b4abb50ee
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-08 12:50:12 +0000

    Format the letters

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 """An application that personalized letter to manual formatting of a large bulk"""
+PLACEHOLDER = "[name]"
 
-# Getting the names of those that qualified
-with open("names/qualified.txt", mode="r") as file:
-    qualified_name = file.readlines()
-    for name in qualified_name:
-        print(name)
+# Get the names of those that qualified
+with open("names/qualified.txt", mode="r") as names_file:
+    qualified_names = names_file.readlines()
+
+# Get the contents of the template letter
+with open("letter_templates/congratulation_template.txt") as template_file:
+    open_template = template_file.read()
+    # Add the name and format the letters
+    for name in qualified_names:
+        name = name.strip()
+        add_names = open_template.replace(PLACEHOLDER, name)
+        print(add_names)
+
