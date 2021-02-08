440f1da73b8cceb87e80a687028589787123e7c4
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-08 13:09:55 +0000

    Add disqualified formatting section

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""QUALIFIED LETTER PERSONALISATION"""
 PLACEHOLDER = "[name]"
 
 # Get the names of those that qualified
@@ -15,3 +16,19 @@ with open("letter_templates/congratulation_template.txt") as template_file:
         with open(f"complete/congratulatory_letters/{name}_letter.txt", mode="w") as format_letter:
             format_letter.write(add_names)
 
+"""DISQUALIFIED LETTER PERSONALISATION"""
+
+# Get the names of those that are disqualified
+with open("names/disqualified.txt", mode="r") as names_file:
+    disqualified_names = names_file.readlines()
+
+# Get the contents of the template letter
+with open("letter_templates/refusal_template.txt") as template_file:
+    open_template = template_file.read()
+    # Add the name and format the letters
+    for name in disqualified_names:
+        name = name.strip()
+        add_names = open_template.replace(PLACEHOLDER, name)
+        # Create the personalised letters
+        with open(f"complete/disqualified_letters/{name}_letter.txt", mode="w") as format_letter:
+            format_letter.write(add_names)
